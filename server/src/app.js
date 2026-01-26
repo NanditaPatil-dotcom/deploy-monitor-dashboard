dbd9f48cc2d599aead1b972764c50fe3b0fae25b
NanditaPatil-dotcom <nanditanpatil14@gmail.com>
2026-01-26 14:23:59 +0530

    adding missing project router and tested screenshot

--- server/src/app.js
+++ server/src/app.js
@@ -1,38 +1,30 @@
 import express from "express";
+
 import healthRouter from "./routes/health.route.js";
-import { errorHandler } from "./middleware/error.middleware.js";
 import authRouter from "./routes/auth.route.js";
-import meRouter from "./routes/me.route.js";
 import projectRouter from "./routes/project.route.js";
 
-
+import { errorHandler } from "./middleware/error.middleware.js";
 
 const app = express();
 
-// built-in middleware
 app.use(express.json());
 
 // routes
 app.use("/health", healthRouter);
-
 app.use("/auth", authRouter);
-
 app.use("/projects", projectRouter);
 
-
-app.use("/", meRouter);
-
-// 404 handler (important)
-app.use((req, res, next) => {
+// 404 handler
+app.use((req, res) => {
   res.status(404).json({
     success: false,
     message: "Route not found",
   });
 });
 
-
-
-// central error handler (MUST be last)
+// error handler LAST
 app.use(errorHandler);
 
 export default app;
+
